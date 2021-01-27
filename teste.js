da563c884d0958a3841d20ac56ecf6f3369c9551
erick santos <kekierick15@gmail.com>
2021-01-26 21:37:10 -0300

    teste

---
+++ teste.js
@@ -0,0 +1,68 @@
+console.log("bem vindo ao sla oq")
+
+console.log(33*73)
+console.log(12.5 / 23)
+console.log(12.9+76)
+
+// NaN not a number 
+// Infinity, not is infinity
+// boolean = true or false
+console.log(true)
+console.log(false)
+
+console.log(undefined)
+// undefined = indefinido
+
+console.log(null)
+// null = nulo, objeto que nao possui nada dentro, diferente de indefinido
+
+console.log({
+    name: "roberta",
+    idade: "27",
+    andar: function() {
+        console.log('andar')
+    }
+
+})
+/* object 
+    * objeto
+    * propriedades / atributos
+    * funcionalidade / metodos 
+     {propriedade: "valor" }
+*/
+
+console.log({"myke": 36})
+
+/* array (vetores)
+  *uma lista 
+  *agrupamento de dados 
+
+  ["myke", 36]
+  */
+
+  /*
+    - Primitivos
+
+    * String
+    * Number
+    * Boolean
+    * Undefined
+    * Symbol
+    * BigInit
+
+    - Estruturais
+
+    * Object
+      * Array
+      * Map
+      * Ser
+      * Date
+      * ...
+    * Function
+    
+    - Primitivo Estrutural
+
+    * Null
+     
+  */
+ 
