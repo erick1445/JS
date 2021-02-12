0a679ebfff48672764dc18445ad5adc8f7fa96b6
erick santos <kekierick15@gmail.com>
2021-02-11 22:48:48 -0300

    estruturarepetição

---
+++ FOR.js
@@ -0,0 +1,53 @@
+// estrutura de repetição 
+// for
+// brak - para a execução do loop 
+// continue - pula a execução do momento 
+
+
+/*
+for(let i = 1; i < 10; i++) {
+      if(i === 5) {
+          continue;
+      }
+      console.log(i);
+} 
+*/
+
+// while 
+
+/*
+let i = 484654684521541645;
+while(i > 10) {
+    console.log(i)
+
+    i /= 35
+}
+*/
+
+
+// for...of
+
+/*
+let name = 'myke'
+let names = ['joao', 'paulo', 'pedro']
+
+for(let name of names){
+    console.log(name)
+}
+*/
+
+
+// for...in
+
+/*
+let person = {
+    name: 'jhon',
+    age: 30,
+    weigth: 88.7
+}
+
+for(let property in person){
+   console.log(property);
+   console.log(person[property]);
+}
+*/
